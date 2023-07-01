d9fd780ee307d4d466387601f17d4c1b6d76b7d0
Michail Chatzipanagiotou <chtzmike@gmail.com>
2023-07-01 16:38:02 +0200

    feat: steps 1 & 2

---
+++ vagrantapps/app1/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;  // change port for each app (e.g., 3001, 3002)
+
+app.get('/', (req, res) => {
+    res.send('Hello from app1!');  // change string for each app
+});
+
+app.listen(port, () => {
+});
